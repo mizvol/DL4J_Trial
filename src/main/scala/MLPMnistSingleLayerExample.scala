4c9f522e5abb10a7b2475ca1499fad7ecc1aafc8
Volodymyr Miz <volodymyr.miz@epfl.ch>
2017-05-26 13:25:26 +0200

    Changed optimizer and learning rate to improve accuracy

--- src/main/scala/MLPMnistSingleLayerExample.scala
+++ src/main/scala/MLPMnistSingleLayerExample.scala
@@ -35,8 +35,8 @@ object MLPMnistSingleLayerExample {
       .seed(rngSeed)
       .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
       .iterations(1)
-      .learningRate(0.006) //specify the learning rate
-      .updater(Updater.NESTEROVS).momentum(0.9) //specify the rate of change of the learning rate.
+      .learningRate(0.06) //specify the learning rate
+      .updater(Updater.ADAM).momentum(0.9) //specify the rate of change of the learning rate.
       .regularization(true).l2(1e-4)
       .list()
       .layer(0, new DenseLayer.Builder() //create the first, input layer with xavier initialization
